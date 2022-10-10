9b5cf12c66d5f0130980f833c52f3974d23727fd
jacobcook135 <jacobcook135@gmail.com>
2022-10-10 19:21:31 -0400

    Link CSS to Django

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -125,5 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'static/css'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
